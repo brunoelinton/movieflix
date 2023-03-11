d5d5bab8202748958964d15be3179ac8986f9c52
brunoelinton <brunoelinton@outlook.com>
2023-03-11 16:09:37 -0300

    Backend: User, Role

--- backend/src/main/java/com/bega/movieflix/MovieflixApplication.java
+++ backend/src/main/java/com/bega/movieflix/MovieflixApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.movieflix;
+package com.bega.movieflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/bega/movieflix/controllers/UserControllerIT.java
+++ backend/src/test/java/com/bega/movieflix/controllers/UserControllerIT.java
@@ -1,10 +1,11 @@
-package com.devsuperior.movieflix.controllers;
+package com.bega.movieflix.controllers;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.bega.movieflix.tests.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.devsuperior.movieflix.tests.TokenUtil;
 
 @SpringBootTest
 @AutoConfigureMockMvc
